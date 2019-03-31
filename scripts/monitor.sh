16b7f8a0622c100f9f1433733627bf72914e0761
Colby Morrison <colbyamorrison@gmail.com>
2019-03-31 17:30:21 -0400

    updates

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -8,11 +8,3 @@ DISPLAY_LN=$(echo "$CON" | head -n $CON_LINES | tail -1)
 
 export DISPLAY_NM=$(echo $DISPLAY_LN | awk '{print $1}')
 export DISPLAY_SZ=$(echo $DISPLAY_LN | awk '{print $3}')
-
-# TODO MAKE THIS BETTER SO IT DOESNT ONLY WORK ON STARTUP
-if [ "${CON_LINES}" -gt 1 ]; then
-   xrandr --output eDP1 --off
-   echo 0 > /sys/devices/platform/applesmc.768/leds/smc::kbd_backlight/brightness
-fi
-
-[ "${DISPLAY_NM}" == "HDMI2" ] && xrandr --output HDMI2 --mode 3840x2160
