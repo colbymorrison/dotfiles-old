11cc32d62628b2edbd8f5cc1daad833aeeb00640
Colby Morrison <colbyamorrison@gmail.com>
2019-04-25 01:17:56 -0400

    good

--- scripts/shell_cwd.sh
+++ scripts/shell_cwd.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-#ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
-#PID=$(($(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3) + 2))
-#if [ -e "/proc/$PID/cwd" ]
-#then
-#    termite -d $(readlink /proc/$PID/cwd) &
-#else
-#    termite &
-#fi
-
-termite -d $(cat /tmp/whereami)
+[[ -f /tmp/whereami ]] && termite -d $(cat /tmp/whereami)
