4d1ff0b7f874f746e111dc03665ec0c0be57b291
Colby Morrison <colbyamorrison@gmail.com>
2020-03-29 17:01:37 -0700

    ------  i3 -> bspwm   ------

--- config/.config/polybar/launch.sh
+++ config/.config/polybar/launch.sh
@@ -10,14 +10,20 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 3; done
 base=/sys/devices/platform/coretemp.0/hwmon
 [[ -d $base/hwmon0 ]] && export HWMON="$base/hwmon0/temp1_input" || export HWMON="$base/hwmon1/temp1_input"
 
+# Wal config file
+source  ~/.cache/wal/polybar-colors.sh
+
 # Start on all monitors with right font size
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    FONT_SZ=10
-    export NOTOT="NotoSans-Regular:size=$FONT_SZ;-1"
-    export MATRT="MaterialIcons:size=$(($FONT_SZ+2));0"
+    # Can change font size by monitor here
+    FONT_SZ=11
+
+    export NOTOT="NotoSans-Bold:size=$FONT_SZ;0"
+    export DEJA="DejaVuSans-Bold:size=$FONT_SZ;1"
     export TERMST="Termsynu:size=$FONT_SZ:antialias=false;-2"
     export AWSM="Font Awesome 5 Free Solid:size=$(($FONT_SZ+2));0"
     export AWSB="FontAwesome 5 Brands Regular:size=$(($FONT_SZ+2));0"
 
     MONITOR=$m polybar --reload top &
 done
+
